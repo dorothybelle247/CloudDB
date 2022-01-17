fc38ade4f51cf3e63dca51e3b436c41e62a22088
Dorothy Ellaine Arman Prince <44729364+dorothybelle247@users.noreply.github.com>
2022-01-17 01:49:14 +0000

    hello

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const exxpress = require("express")
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello there")
+})
+
+app.listen(3000, () => {console.log(`server is running on http://localhost:$(3000)`)})
